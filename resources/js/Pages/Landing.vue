3b7e74649dd37c167c5ae4dcf1d7e0fe219d100b
piet0056 <piet0056@algonquinlive.com>
2023-07-14 13:42:54 -0400

    Added first custom page / route

---
+++ resources/js/Pages/Landing.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Landing" />
+
+    <div class="grid grid-cols-2 py-5 px-5"> 
+        <div> 1 </div>
+        <div> 2 </div>
+    </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/landing', function() {
+    return Inertia::render('Landing');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
