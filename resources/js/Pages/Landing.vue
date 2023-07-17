7911a9aed208f42c2cf7c86245c1b574807a70bd
piet0056 <piet0056@algonquinlive.com>
2023-07-17 10:29:27 -0400

    Custom font working, landing page v1.1 done
    sort of responsive? some weird stuff at min screen width but  better

--- resources/js/Pages/Landing.vue
+++ resources/js/Pages/Landing.vue
@@ -10,9 +10,9 @@ import { Head } from '@inertiajs/vue3';
 
 
 
-    <div class="grid h-screen font-Poppins" style="background-image: url('/img/landing-bg-day.jpg');">
-        <div class="py-6 px-5 my-auto w-1/3 bg-white rounded-md h-screen">
-            <div class="text-3xl mb-10 my-10 mx-auto text-center">Welcome</div>
+    <div class="grid h-screen font-Poppins bg-cover" style="background-image: url('/img/landing-bg-day.jpg');">
+        <div class="py-20 px-5 my-auto w-1/3 bg-white rounded-md h-screen min-w-max">
+            <div class="text-3xl mb-10 my-10 mx-auto text-center">Welcome!</div>
             <form>
                 <div class="grid grid-cols-1 w-2/3 mx-auto">
                     <div class="grid grid-cols-1">
@@ -25,27 +25,27 @@ import { Head } from '@inertiajs/vue3';
                         <label class="ml-2" for="pass"> Password </label>
                         <input type="password"
                             class="form-input h-12 border-violet-600 border-4 mx-auto px-3 py-1 mb-3 w-full rounded-full"
-                            id="pass" placeholder="********">
+                            id="pass" placeholder="••••••••••">
                     </div>
+                    <div class="grid w-full grid-cols-2 w-full">
+                    <div class="mb-4 min-w-max">
+                        <input type="checkbox" class="form-checkbox ml-5" id="remember"> <label for="remember"
+                            class="ml-2 mb-4 text-sm"> Remember Me </label>
+                    </div>
+                    <div class="mb-4 text-right text-sm">
+                        <a href="/landing"> <span class="italic"> Forgot Password? </span> </a>
+                    </div>
+                </div>
                 </div>
                 <div class="grid grid-cols-1 w-full mx-auto mb-10">
                     <button
                         class="rounded-full bg-violet-600 h-12 focus:ring focus:ring-violet-300 w-48 mx-auto text-white ">Login</button>
                 </div>
-                <div class="grid w-full grid-cols-2 my-4">
-                    <div class="mb-4">
-                        <input type="checkbox" class="form-checkbox ml-5" id="remember"> <label for="remember"
-                            class="ml-2 mb-4 text-sm"> Remember Me </label>
-                    </div>
-                    <div class="mb-4 text-center text-sm">
-                        <span class="italic"> Forgot Password? </span>
-                    </div>
-                </div>
 
             </form>
             <hr class="mb-10">
             <div class="text-center mx-auto mt-3 italic font-thin"> No account? No worries.</div>
-            <div class="mx-auto w-full grid"> <button class="mx-auto w-48 form-input rounded-full"> Join Now </button>
+            <div class="mx-auto w-full grid"> <button class="mx-auto w-48 hover:bg-violet-600/50 form-input rounded-full"> Join Now </button>
             </div>
         </div>
     </div>
