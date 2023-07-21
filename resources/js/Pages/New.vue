75bd442b4d2cb4dd93acf5735b574cab70aa26c5
haseeb8143 <haseebiqbal8143@gmail.com>
2023-07-20 23:42:28 -0500

    New task page - nav component added in components

--- resources/js/Pages/New.vue
+++ resources/js/Pages/New.vue
@@ -1,41 +1,69 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
+import Navigation from '@/Components/Navigation.vue';
 </script>
 <template>
     <Head title="New" ></Head>
+    <Navigation></Navigation>
     
-
-    <div class="ml-7 text-4xl m-4">
+    <div class="flex justify-evenly flex-col items-center h-screen bg-cover" style="background-image: url('/img/purple nature bg.jpg');">
+    <div class="text-4xl text-white">
       <h1>
         Add a New Task
       </h1>
     </div>
-<div style="background-image: url('resources/css/img/purple nature bg.jpg');">
 
-
-    <div class="w-1/2 bg-gray-100 drop-shadow-xl rounded mx-auto">
-      <form class="rounded px-8 pt-6 pb-8 mb-4">
+    <div class="w-2/5 bg-gray-100 drop-shadow-xl rounded-lg">
+      <form class=" px-8 pt-6 pb-8">
               <div class="mb-6">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="title">
                   Title
                 </label>
                 <input class="shadow appearance-none border border-purple-500 rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="title" type="text">
               </div>
+              
               <div class="mb-6">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="description">
                   Description
                 </label>
-                <input class="shadow appearance-none border border-purple-500 rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="description" type="text">
+                <textarea name="des" id="description" cols="30" rows="2" style="border-radius: 15px; resize: none;scrollbar-width: none ;" class="shadow appearance-none border border-purple-500 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+
+                </textarea>               
               </div>
-              <div class="mb-6">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="description">
-                  Priority
-                </label>
-                <input class="shadow appearance-none border border-purple-500 rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="description" type="text">
-              </div>
+
+              <div class="mb-6 flex justify-start">
+                
+
+                <div>
+                  <label class="block text-gray-700 text-sm font-bold mb-2" for="description">
+                      Start date
+                  </label>
+                  <input type="date" class="shadow appearance-none border border-purple-500 rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="start date">
+                </div>
+
+                <div class="pl-8">
+                  <label class="block text-gray-700 text-sm font-bold mb-2" for="description">
+                      Due date
+                  </label>
+                  <input type="date" class="shadow appearance-none border border-purple-500 rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="start date">
+                </div>
+             </div>
+
+             <div class="mb-6">
+                  <label class="block text-gray-700 text-sm font-bold mb-2" for="description">
+                    Priority
+                  </label>
+                  <select class="shadow appearance-none border border-purple-500 rounded-full w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="priority">
+                    <option value="0">Select</option>
+                    <option value="low">Low</option>
+                    <option value="mid">Medium</option>
+                    <option value="high">High</option>
+                  </select>
+                </div>
+
               <div class="flex items-center justify-between">
-                <button class="button bg-purple-400 hover:bg-yellow-300 text-black font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline" type="button">
-                  Add
+                <button class="button py-2 px-4 rounded-full focus:outline-none focus:shadow-outline" type="button">
+                  Add Task
                 </button>
               </div>
             </form>
