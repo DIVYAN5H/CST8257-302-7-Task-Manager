da3e15dd531793a1781b517dfbee21ce77ad4717
piet0056 <piet0056@algonquinlive.com>
2023-07-30 21:46:32 -0400

    Basic Task Components Updated
    you can click the task to strike it through
    you can click the badge in the top left to change its colour (just 2 rn)

---
+++ resources/js/Components/TaskBadge.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { usePage } from '@inertiajs/vue3';
+import { ref } from 'vue';
+defineProps(['colour']);
+
+const page = usePage();
+const colour = ref('true');
+
+
+</script>
+
+<template>
+
+<div class="flex text-lg font-bold">
+    <button @click="colour = !colour" :class=" colour ? 'bg-white ring-2 ring-green-500' : 'bg-black'" class="rounded rounded-full  w-2 h-2 my-auto mr-2">
+    </button>
+<slot></slot>
+</div>
+
+
+</template>
--- resources/js/Components/TaskComponent.vue
+++ resources/js/Components/TaskComponent.vue
@@ -1,14 +1,27 @@
 <!-- BlogPost.vue -->
 <script setup>
+import TaskBadge from '@/Components/TaskBadge.vue';
+import { ref } from 'vue';
+
+const completed=ref(false);
+
 defineProps(['title', 'body', 'priority'])
 
 </script>
 
 <template>
-    <div  class="backdrop-blur w-[18.8rem] bg-white/30 rounded p-2 mx-2 my-4 box-borders">
+    <div class="backdrop-blur w-[18.8rem] bg-white/20 rounded-lg  mb-8 ">
         <div class="text-white ">
-            <h4> {{ title }} </h4>
-            <span> {{ body }} </span>
+            <div class="grid grid-cols-1"> 
+                <div class="bg-white/50 h-full rounded-t-lg px-3">
+                    <TaskBadge :colour="'bg-white'"> {{ title }}  </TaskBadge>
+                </div>
+
+
+            </div>
+            <div class="text-xs pl-4 mt-1"> Due: 10/23/23 </div>
+            <div @click="completed = !completed" :class="completed ? 'line-through' : '' " class="hover:bg-white/30 select-none rounded-b-lg text-base cursor-pointer pl-4"> {{ body }} </div>
+
         </div>
 
 
