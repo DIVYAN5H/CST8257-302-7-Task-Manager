38e2810e4427212a568ab9780809e1eeb29cfaf5
piet0056 <piet0056@algonquinlive.com>
2023-07-30 20:47:51 -0400

    Samll Visual Changes

--- resources/js/Components/UserFlyout.vue
+++ resources/js/Components/UserFlyout.vue
@@ -10,8 +10,8 @@ const form = useForm({
 </script>
 
 <template>
-    <div class="h-screen text-white w-full bg-texture-1">
-        <div class="grid-cols-1 mx-auto w-full h-full py-16">
+    <div class="h-screen text-white w-full bg-texture-1 overflow-hidden">
+        <div class="grid-cols-1 mx-auto w-full h-full py-16 overflow-hidden">
             <div>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-48 h-48 mx-auto border-4 rounded-full p-2">
@@ -26,14 +26,14 @@ const form = useForm({
                 0 Tasks Completed
             </div>
             <div id="userInfoSection" class="grid grid-cols-2">
-                <div class="grid-cols-1 text-lg w-2/3 mx-auto py-16">
+                <div class="grid-cols-1 text-lg w-1/2 mx-auto py-16">
                     <div> Email: </div>
                     <div> Username: </div>
                     <form @submit.prevent="form.post('/logout1')">
                         <button type="submit"> Logout </button>
                     </form>
                 </div>
-                <div class="grid-cols-1 text-lg w-2/3 mx-auto py-16">
+                <div class="grid-cols-1 text-lg w-1/2 py-16">
                     <div> {{ email }}</div>
                     <div>{{ username }}</div>
                     <form @submit.prevent="form.post('/logout1')">
@@ -42,7 +42,5 @@ const form = useForm({
                 </div>
             </div>
         </div>
-
-
     </div>
 </template>
