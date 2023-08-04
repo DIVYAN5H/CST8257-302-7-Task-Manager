be58827988404391e80fd3c40718e6b26673b19a
Divyansh Verma <divyanshvermafast4@gmail.com>
2023-08-04 15:33:31 -0400

    list color

--- resources/js/Components/TaskBadge.vue
+++ resources/js/Components/TaskBadge.vue
@@ -11,10 +11,12 @@ const page = usePage();
 <template>
 
 <div class="flex text-4xl font-bold">
-    <button @click="color = !color" :class=" color ? 'bg-white ring-4 ring-['+ color +']' : 'bg-black ring-4 ring-['+color+']'" class="rounded rounded-full  w-2 h-2 my-auto mr-2">
-    </button>
-    <div :class="['rounded rounded-full w-5 h-5 my-auto mr-5', 'bg-['+color+']']">
-    </div>
+    <!-- <button @click="color = !color" :class=" color ? 'bg-white ring-4 ring-['+ color +']' : 'bg-black ring-4 ring-['+color+']'" class="rounded rounded-full  w-2 h-2 my-auto mr-2">
+    </button> -->
+    <div
+      :style="{ border: '5px solid'+ color }"
+      class="rounded rounded-full w-5 h-5 my-auto mr-5"
+    ></div>
 <slot></slot>
 <div class="text-base my-1 pl-4 absolute right-0 mr-3 "> Due: 10/23/23 </div>
 </div>
