23d7c0979832fb99c91c80b8444c312e75b47602
haseeb8143 <haseebiqbal8143@gmail.com>
2023-08-11 13:22:10 -0400

    errors have been added

--- resources/js/Components/NewList.vue
+++ resources/js/Components/NewList.vue
@@ -11,6 +11,7 @@ const user = page.props.user;
 const email = page.props.email
 const name = page.props.name;
 const password = page.props.password
+const errors = page.props.errors;
 
 console.log(password);
 
@@ -36,6 +37,7 @@ const form = useForm({
                     <input v-model="form.list" name="list"
                         class="rounded rounded-md shadow appearance-none w-full text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         id="list" type="text" />
+                        <div v-if="errors.list" class="text-xs text-red-900 px-2 mb-2">{{ errors.list }}</div>
                 </div>
                 <div class="col-span-1">
                     <label class="block  text-sm font-bold my-1" for="description">
@@ -48,6 +50,7 @@ const form = useForm({
                         <option selected value="2">Medium</option>
                         <option value="3">High</option>
                     </select>
+                    <div v-if="errors.priority" class="text-xs text-red-900 px-2 mb-2">{{ errors.priority }}</div>
                 </div>
                 <div class="col-span-2">
 
@@ -57,6 +60,7 @@ const form = useForm({
                     <input type="date" v-model="form.date" name="date" placeholder="dd-mm-yy"
                         class="rounded-md w-full shadow appearance-none text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         id="date" />
+                        <div v-if="errors.date" class="text-xs text-red-900 px-2 mb-2">{{ errors.date }}</div>
                 </div>
                 <div class="col-span-1 group">
                     <label class="block text-sm font-bold mb-2" for="description">
@@ -67,6 +71,7 @@ const form = useForm({
                             class="rounded-full h-10 z-10 w-full shadow appearance-none text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
                         <ColourPickComponent class="opacity-0 scale-0 absolute inset-0 z-0 transition duration-200 group-hover:opacity-100 group-hover:scale-100"
                             @colour="setColour"></ColourPickComponent>
+                            <div v-if="errors.color" class="text-xs text-red-900 px-2 mb-2">{{ errors.color }}</div>
                     </div>
 
                 </div>
